056f0f0c0a41aec37fa09cb438e8a44a704bf635
HY <iyongyong@126.com>
2019-05-02 22:43:00 +0800

    测试Math类（侧重方法类型的类）

---
+++ Class/MethodSample/MethodSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Math.Sqrt(4);
+            Console.WriteLine(x);
+        }
+    }
+}
