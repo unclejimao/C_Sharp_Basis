9f4e37094d45b7a18dfca525c1e520c9de26f181
HY <iyongyong@126.com>
2019-05-02 22:39:24 +0800

    测试Entity FrameWork类

---
+++ Class/PropertySample/PropertySample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //教程实例用到了老师本地的sqlserver数据库，此处无法实验
+        }
+    }
+}
