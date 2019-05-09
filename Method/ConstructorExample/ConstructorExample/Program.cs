5984bacb73e293a23d570ef3ba9bc4b671dfc93f
HY <iyongyong@126.com>
2019-05-09 18:06:58 +0800

    更新

--- Method/ConstructorExample/ConstructorExample/Program.cs
+++ Method/ConstructorExample/ConstructorExample/Program.cs
@@ -17,7 +17,29 @@ namespace ConstructorExample
             Student student2 = new Student();
             Console.WriteLine(student2.ID);
             Console.WriteLine(student2.Name);
-            Console.WriteLine()
+
+            Calulator c = new Calulator();
+            int r = c.Add(1, 2, 3);
+            Console.WriteLine("====================");
+            Console.WriteLine(r);
+        }
+    }
+
+    class Calulator
+    {
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+
+        public double Add(double a,double b)
+        {
+            return a + b;
+        }
+
+        public int Add(int a,int b,int c)
+        {
+            return a + b + c;
         }
     }
 
