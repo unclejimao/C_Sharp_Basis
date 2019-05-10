fbb7585d5f013068ad840f8a1ad6ce1ad42d601c
HY <iyongyong@126.com>
2019-05-10 17:24:55 +0800

    update

--- Operator/ConversionExample/ConversionExample/Program.cs
+++ Operator/ConversionExample/ConversionExample/Program.cs
@@ -10,12 +10,41 @@ namespace ConversionExample
     {
         static void Main(string[] args)
         {
-            string str1 = Console.ReadLine();
-            string str2 = Console.ReadLine();
-            Console.WriteLine(str1 + str2);
-            int x = Convert.ToInt32(str1);
-            int y = Convert.ToInt32(str2);
-            Console.WriteLine(x + y);
+            //string str1 = Console.ReadLine();
+            //string str2 = Console.ReadLine();
+            //Console.WriteLine(str1 + str2);
+            //int x = Convert.ToInt32(str1);
+            //int y = Convert.ToInt32(str2);
+            //Console.WriteLine(x + y);
+
+            Teacher t = new Teacher();
+            Human h = t;
+            Animal a = h;
+            a.Eat();
+        }
+    }
+
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+
+    class Human : Animal
+    {
+        public void Think()
+        {
+            Console.WriteLine("Thinking...who am I?");
+        }
+    }
+
+    class Teacher : Human
+    {
+        public void Teach()
+        {
+            Console.WriteLine("Teaching...");
         }
     }
 }
