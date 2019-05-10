789c169b012d9fc5912ed76d6523a2e7bcee5dfd
HY <iyongyong@126.com>
2019-05-10 17:06:28 +0800

    update

---
+++ Operator/ConversionExample/ConversionExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+            Console.WriteLine(str1 + str2);
+            int x = Convert.ToInt32(str1);
+            int y = Convert.ToInt32(str2);
+            Console.WriteLine(x + y);
+        }
+    }
+}
